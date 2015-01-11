4adb9f254076bff8387c24b39c035039e1751f66
Eduardo Díaz <iamedu@gmail.com>
2015-01-11 10:13:25 -0600

    Basic raml structure is ready

--- templates/rest-boot-api/lazybones.groovy
+++ templates/rest-boot-api/lazybones.groovy
@@ -3,4 +3,9 @@ props.group = ask("Define value for 'group' [gex.example]: ", "gex.example", "gr
 props.projectName = ask("Define value for 'projectName' [sample-project]: ", "sample-project", "projectName")
 props.version = ask("Define value for 'version' [0.1.0-SNAPSHOT]: ", "0.1.0-SNAPSHOT", "version")
 
+props.groupFolder = props.group.replaceAll('\\.', '/')
+
 processTemplates "gradle.properties", props
+processTemplates "raml/package.json", props
+processTemplates "raml/Gulpfile.js", props
+processTemplates "raml/src/index.raml", props
---
+++ templates/rest-boot-api/raml/Gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+
+var raml2code = require("raml2code/index.js");
+var genPojos = require("raml2code/lib/generators/groovy/pojo.js");
+var genJaxRs = require("raml2code/lib/generators/groovy/jaxrsInterface.js");
+var genRetrofit = require("raml2code/lib/generators/groovy/retrofitClient.js");
+
+
+var raml = require('gulp-raml');
+var packagePojo = "${group}.dto";
+var packageJaxRs = "${group}.jaxrs";
+var packageClient = "${group}.client";
+
+gulp.task('raml', function() {
+    gulp.src('./src/index.raml')
+        .pipe(raml())
+        .pipe(raml.reporter('default'));
+});
+
+
+gulp.task("genPojos", ['raml'], function(){
+    gulp.src('./src/index.raml')
+        .pipe(raml2code({generator: genPojos, extra: {package: packagePojo}}))
+        .pipe(gulp.dest('../commons/src/generated/groovy/${groupFolder}/dto'));
+});
+
+gulp.task("genJaxRs" , ['raml'], function(){
+    gulp.src('./src/index.raml')
+        .pipe(raml2code({generator: genJaxRs, extra: {package: packageJaxRs, importPojos: packagePojo}}))
+        .pipe(gulp.dest('../api/src/generated/groovy/${groupFolder}/jaxrs'));
+});
+
+
+gulp.task("genClient" , ['raml'], function(){
+    gulp.src('./src/index.raml')
+        .pipe(raml2code({generator: genRetrofit, extra: {package: packageClient, importPojos: packagePojo}}))
+        .pipe(gulp.dest('../client/src/generated/groovy/${groupFolder}/client'));
+});
+
+
+gulp.task('apidoc', function() {
+    return gulp.src('./src/*')
+        .pipe(raml2html())
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['raml', 'genPojos', 'genJaxRs', 'genClient' ]);
