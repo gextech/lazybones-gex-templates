0d83ab6ba2c5f610f75685b15a82e4a9d265865e
Eduardo Díaz <iamedu@gmail.com>
2015-01-11 10:42:06 -0600

    Adding lazybones app

---
+++ templates/rest-boot-api/api/src/main/groovy/groupFolder/Application.groovy
@@ -0,0 +1,13 @@
+package ${group}
+
+import org.springframework.boot.autoconfigure.SpringBootApplication 
+
+import static org.springframework.boot.SpringApplication.run
+
+@SpringBootApplication
+class Application {
+  static void main(String[] args) {
+    run Application, args
+  }
+}
+
--- templates/rest-boot-api/lazybones.groovy
+++ templates/rest-boot-api/lazybones.groovy
@@ -1,11 +1,22 @@
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.FilenameUtils
+
 def props = [:]
 props.group = ask("Define value for 'group' [gex.example]: ", "gex.example", "group")
 props.projectName = ask("Define value for 'projectName' [sample-project]: ", "sample-project", "projectName")
 props.version = ask("Define value for 'version' [0.1.0-SNAPSHOT]: ", "0.1.0-SNAPSHOT", "version")
 
-props.groupFolder = props.group.replaceAll('\\.', '/')
+props.groupFolder = props.group.replace('.' as char, '/' as char)
+
 
 processTemplates "gradle.properties", props
 processTemplates "raml/package.json", props
 processTemplates "raml/Gulpfile.js", props
 processTemplates "raml/src/index.raml", props
+processTemplates "api/src/main/groovy/**/*.groovy", props
+
+File originApiFolder = new File(projectDir, "api/src/main/groovy/groupFolder/")
+File destFolder = new File(projectDir, FilenameUtils.concat("api/src/main/groovy/", props.groupFolder))
+destFolder.parentFile.mkdirs()
+
+FileUtils.moveDirectory(originApiFolder, destFolder)
