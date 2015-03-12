1af4c7aaa32923c97988675d63410292db99d3e5
Anallely Olivares Toledo <anaoko@gmail.com>
2015-03-11 18:13:10 -0600

    test added.

--- templates/rest-boot-api/client/src/test/groovy/groupFolder/HeroSpec.groovy
+++ templates/rest-boot-api/client/src/test/groovy/groupFolder/HeroSpec.groovy
@@ -56,4 +56,31 @@ class HeroSpec extends Specification{
       apiClient.deleteHero('Goku').toBlocking().first()
   }
 
+  def 'GET: Hero pagination works'(){
+    setup:
+      Hero hero = new Hero(name: 'Goku', age: 12, immortal: false)
+      apiClient.postHeroes(hero).toBlocking().first()
+
+      hero = new Hero(name: 'Vegeta', age: 12, immortal: false)
+      apiClient.postHeroes(hero).toBlocking().first()
+    when:
+      def response = apiClient.getHeroesAndSizeAndFrom(1, 0).toBlocking().first()
+
+
+    then:
+      response.items
+      response.items.size() == 1
+      response.items[0].class == Hero.class
+      response.page.from == 0
+      response.page.size == 1
+      response.page.currentPage == 1
+      response.page.total >= 2
+      response.page.hasPrev == false
+      response.page.hasNext == true
+
+    cleanup:
+      apiClient.deleteHero('Goku').toBlocking().first()
+      apiClient.deleteHero('Vegeta').toBlocking().first()
+  }
+
 }
